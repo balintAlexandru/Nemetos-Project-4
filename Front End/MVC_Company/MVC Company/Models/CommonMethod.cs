7e06d7f7329a92f91b5a0680c86889f35a7c20a6
Alexandru <balintalex737@gmail.com>
2021-11-17 13:48:56 +0200

    Changed

---
+++ Front End/MVC_Company/MVC Company/Models/CommonMethod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Company.Models
+{
+    public class CommonMethod
+    {
+        public string Key = "adef@@kfxcbv@";
+        public string ConvertToEncrypt(string password)
+        {
+            if (string.IsNullOrEmpty(password)) return "";
+            password += Key;
+            var passwordBytes = Encoding.UTF8.GetBytes(password);
+            return Convert.ToBase64String(passwordBytes);
+        }
+        public string ConvertToDecrypt(string base64EncodeData)
+        {
+            if (string.IsNullOrEmpty(base64EncodeData)) return "";
+            var base64EncodeBytes = Convert.FromBase64String(base64EncodeData);
+            var result = Encoding.UTF8.GetString(base64EncodeBytes);
+            result = result.Substring(0, result.Length - Key.Length);
+            return result;
+        }
+    }
+}
